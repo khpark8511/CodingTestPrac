31fa15000d57577960d418e629dcb7c538d20eeb
khpark8511 <khpark8511@naver.com>
2023-06-04 18:28:25 +0900

    [Silver II] Title: 유기농 배추, Time: 200 ms, Memory: 116368 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,38 +1,53 @@
-import sys
-sys.setrecursionlimit(2501)
-# 1012 유기농 배추 > 인접 
+from collections import deque
 t = int(input())
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+cnt = 0
 
-def dfs(x,y):
-    if x < 0 or y < 0 or x >= n or y >= m:
-        return False
-    if graph[x][y] == 0:
-        return False
-    if graph[x][y] == 1:
-        graph[x][y] = 0 
-        dfs(x-1,y)
-        dfs(x,y-1)
-        dfs(x+1,y)
-        dfs(x,y+1)
-        return True
-    return False
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x,y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue 
+            if graph[nx][ny] == 0 :
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 2
+                queue.append((nx,ny))        
+    return 1 
 
 
 for _ in range(t):
-    res = 0 
+    cnt = 0
     m,n,k = map(int,input().split())
-    graph = [[0]*m for _ in range(n)]
+    graph = [[0]*m for x in range(n)]
 
-    for i in range(k):
-        x, y = map(int,input().split())
-        graph[y][x] = 1 
-    
-    for l in range(n):
+    for _ in range(k):
+        x,y = map(int,input().split())
+        graph[y][x] = 1
+
+    for i in range(n):
         for j in range(m):
-            if dfs(l,j) == True:
-                res += 1
-    print(res)
+            if graph[i][j] == 1 : 
+                cnt += bfs(i,j)
+    print(cnt)
+
+
+
 
 
     
+
+
+
+
+
 
