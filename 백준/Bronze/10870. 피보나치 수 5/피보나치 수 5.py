010027d1881a8165526e4438b50f1ee2b095a249
khpark8511 <khpark8511@naver.com>
2023-06-12 00:32:07 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,12 @@
+import sys 
+sys.setrecursionlimit(10**6)
+n = int(input())
+
+def fibo(x):
+    if x == 0:
+        return 0 
+    elif x == 1 : 
+        return 1 
+    else:
+        return fibo(x-1)+fibo(x-2)
+print(fibo(n))
