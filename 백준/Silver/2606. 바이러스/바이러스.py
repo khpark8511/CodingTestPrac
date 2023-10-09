661cb4392893bff5f32f1ad3bdd87b46ec27dee9
gg <khpark8511@naver.com>
2023-10-09 23:09:59 +0900

    [Silver III] Title: 바이러스, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,31 +1,25 @@
-#virus 
-from collections import deque
+n = int(input())
+m = int(input())
+graph = [[] for x in range(n+1)]
+visited = [0]*(n+1)
 
-com = int(input())
-con = int(input())
-visited=[False]*(com+1)
-graph= [[] for x in range(com+1)]
-
-for i in range(con):
+global cnt 
+cnt = 0 
+for i in range(m):
     a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-    graph[a].sort()
-    graph[b].sort()
-#print(graph)
 
-def bfs(graph,v,visited):
-    cnt = 0
-    queue = deque([v])
-    visited[v] = True 
-    queue.append(v)
-    while queue:
-        n = queue.popleft()
-        for i in graph[n]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
-                cnt += 1
+for i in range(n+1):
+    graph[i].sort()
+
+def dfs(x):
+    global cnt
+    visited[x] = True
+    for i in graph[x]:
+        if not visited[i] : 
+            dfs(i)            
+            cnt += 1 
     return cnt
 
-print(bfs(graph,1,visited))
+print(dfs(1))
