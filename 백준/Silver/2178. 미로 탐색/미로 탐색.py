891ef1928c622460d2bd1cc993175f2680cc04f7
gg <khpark8511@naver.com>
2023-10-19 22:37:54 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 116508 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,10 +1,13 @@
-from collections import deque
+import sys
+from collections import deque 
 
+input = sys.stdin.readline
 n,m = map(int,input().split())
-graph = [[] for x in range(n)]
-for i in range(n):
-    graph[i] = list(map(int,input()))
-#print(graph)
+graph = [] # 입력받을 숫자
+
+for _ in range(n):
+    graph.append(list(map(int,input().rstrip())))
+
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
@@ -13,19 +16,12 @@ def bfs(x,y):
     queue.append((x,y))
     while queue:
         x,y = queue.popleft()
-        #print(x,y,end=' ')
-        #print(graph[x][y])
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            if graph[nx][ny] == 0 :
-                continue    
-            if graph[nx][ny] == 1 :
-                graph[nx][ny] = graph[x][y] + 1
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==1:
                 queue.append((nx,ny))
-    return graph[n-1][m-1] 
-        
-print(bfs(0,0))
+                graph[nx][ny] = graph[x][y] + 1
+    return graph[n-1][m-1]
+print(bfs(0,0))                        
 
