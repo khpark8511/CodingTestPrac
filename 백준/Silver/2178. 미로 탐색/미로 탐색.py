9b29b9c56d4525fb060cbb3c10d6ebe3ba3b9f5e
gg <khpark8511@naver.com>
2023-10-19 22:46:08 +0900

    [Silver I] Title: 미로 탐색, Time: 152 ms, Memory: 114148 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,15 +1,13 @@
-import sys
-from collections import deque 
+### 231019 > 2178_mazesearch ###
+from collections import deque
 
-input = sys.stdin.readline
 n,m = map(int,input().split())
-graph = [] # 입력받을 숫자
-
-for _ in range(n):
-    graph.append(list(map(int,input().rstrip())))
-
+graph = []#[[0]*m for x in range(n)]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
+
+for i in range(n):
+    graph.append(list(map(int,input())))
 
 def bfs(x,y):
     queue = deque()
@@ -19,9 +17,10 @@ def bfs(x,y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==1:
+
+            if 0 <= nx < n and 0<= ny < m and graph[nx][ny] == 1:
                 queue.append((nx,ny))
                 graph[nx][ny] = graph[x][y] + 1
     return graph[n-1][m-1]
-print(bfs(0,0))                        
+print(bfs(0,0)) 
 
