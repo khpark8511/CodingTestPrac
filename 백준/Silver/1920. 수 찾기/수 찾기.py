197b041bf666e13b00cba46ca1034a645369966c
gg <khpark8511@naver.com>
2023-10-28 19:45:45 +0900

    [Silver IV] Title: 수 찾기, Time: 252 ms, Memory: 135916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -3,15 +3,21 @@ arr = list(map(int,input().split()))
 m = int(input())
 arr2 = list(map(int,input().split()))
 arr.sort()
-def binSearch(arr,target,start,end):
+
+def bin_srh(arr,target,start,end):
     while start <= end:
-        mid = (start+end) // 2 
-        if target == arr[mid]:
-            return 1 
-        elif target > arr[mid] : 
-            start = mid + 1 
-        elif target < arr[mid] : 
-            end = mid - 1 
-    return 0
-for i in arr2:
-    print(binSearch(arr,i,0,len(arr)-1))
+        mid = (start+end)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            end = mid-1
+        else:
+            start =mid+1
+    return None
+
+for x in arr2:
+    res = bin_srh(arr,x,0,n-1)
+    if res != None:
+        print(1)
+    else:
+        print(0)
