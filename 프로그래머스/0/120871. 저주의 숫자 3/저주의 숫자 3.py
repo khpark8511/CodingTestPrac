24a14b5b98dc65acf66a13af194da3a1de4d39c2
gg <khpark8511@naver.com>
2024-06-16 16:49:23 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,10 +1,7 @@
 def solution(n):
-    res = [0 if i != 1 and i !=2 else i for i in range(0,n+1)]
-    if n <= 2 : return res[n]
-    else:
-        for i in range(3,n+1):
-            tmp,idx= 1,1 
-            while '3' in str(res[i-1]+tmp) or (res[i-1]+tmp)%3 ==0 :
-                tmp += 1 
-            res[i] = res[i-1]+tmp
-    return res[n]
+    res = 0 
+    for _ in range(n):
+        res += 1 
+        while '3' in str(res) or res%3 ==0 :
+            res += 1 
+    return res
