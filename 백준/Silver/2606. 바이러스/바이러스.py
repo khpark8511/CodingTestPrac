138e884844d1417f7013574453df9343fda7fad9
gg <khpark8511@naver.com>
2024-12-08 20:47:46 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,25 +1,27 @@
+import sys 
+
+input = sys.stdin.readline
+
 n = int(input())
 m = int(input())
 graph = [[] for x in range(n+1)]
 visited = [0]*(n+1)
-
 global cnt 
 cnt = 0 
+
+def dfs(x):
+    global cnt 
+    if not visited[x] :
+        visited[x] = 1
+        cnt += 1 
+        for i in graph[x] : 
+            if not visited[i] : 
+                dfs(i)
+    return cnt 
+
 for i in range(m):
     a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-for i in range(n+1):
-    graph[i].sort()
-
-def dfs(x):
-    global cnt
-    visited[x] = True
-    for i in graph[x]:
-        if not visited[i] : 
-            dfs(i)            
-            cnt += 1 
-    return cnt
-
-print(dfs(1))
+print(dfs(1)-1)
