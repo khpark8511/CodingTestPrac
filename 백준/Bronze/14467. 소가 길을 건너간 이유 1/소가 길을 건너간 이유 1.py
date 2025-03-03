c038f76c9703992aae2ded4b2423cecbd85135df
gg <khpark8511@naver.com>
2025-03-03 19:33:18 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -1,14 +1,12 @@
+arr = [-1]*10
+tot = 0 
 n = int(input())
-num = [0]*11
-chk = [0]*11
-cnt = 0 
 for i in range(n):
     a,b = map(int,input().split())
-    if chk[a-1] == 0 : 
-        num[a-1] = b 
-        chk[a-1] += 1 
-    elif num[a-1] != b :
-        num[a-1] = b 
-        chk[a-1] += 1 
-        cnt += 1 
-print(cnt)
+    if arr[a-1] != -1 and arr[a-1] != b : 
+        tot += 1 
+        arr[a-1] = b 
+    elif arr[a-1] == -1 : 
+        arr[a-1] = b 
+print(tot)
+        
